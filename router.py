c676225b79ccfb077c14f63f99224ffdedb4a9eb
Alex Vierich <avierich@gmail.com>
2017-03-21 19:05:49 -0400

    Started using pygame

--- router.py
+++ router.py
@@ -1,19 +1,18 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import pygame
 
 class Ant : 
 
     def __init__(self, position, destination) :
+        self.step_length = 15
         self.position = position
         self.destination = destination
         self.connected = False
-        self.path = [[np.random.rand(),np.random.rand()],[np.random.rand(),np.random.rand()]]
-        self.line = plt.plot([],[])
+        self.path = np.array([[position[0],position[1]]])
+        self.color = (np.random.rand()*255,np.random.rand()*255,np.random.rand()*255)
 
-    def get_line() :
-        self.line.set_data(self.path)
-        return self.line
+    def march(self):
+        self.path = np.append(self.path, [[self.path[-1][0]+(1.0 - 2*np.random.rand())*self.step_length,self.path[-1][1]+(1.0 - 2*np.random.rand())*self.step_length]], axis=0)
 
 class Nest :
 
@@ -26,34 +25,11 @@ class Nest :
         self.connections.append(connectedNest)
         self.ants.append(Ant(self.position,connectedNest))
 
-def update_line(num, data, line):
-    line.set_data(data[...,:num])
-    print(line)
-    return line,
 
-def rando_walk(num, data, datatwo, line, linetwo):
-    data[0].append(np.random.rand())
-    data[1].append(np.random.rand())
-    line.set_data(data)
-    datatwo[0].append(np.random.rand())
-    datatwo[1].append(np.random.rand())
-    linetwo.set_data(datatwo)
-    return line,linetwo
-
-def step_world(num, ants) :
-    lines = []
-    for ant in ants :
-        lines.append(ant.get_line)
-
-    return lines
-
-fig1 = plt.figure()
 
 # Setup world
 ants = []
-
-pins = [(0.5,0.5),(0.8,0.5),(0.5,0.8)]
-nests = [Nest((0.2,0.8),ants),Nest((0.8,0.8),ants),Nest((0.2,0.2),ants),Nest((0.8,0.2),ants)]
+nests = [Nest((100,100),ants),Nest((400,100),ants),Nest((100,400),ants),Nest((400,400),ants)]
 
 # Connect pins (This is not a pretty way to acheive this)
 nests[0].addConnection(nests[1])
@@ -61,20 +37,27 @@ nests[1].addConnection(nests[0])
 nests[2].addConnection(nests[3])
 nests[3].addConnection(nests[2])
 
+pygame.init()
+screen = pygame.display.set_mode((640,480))
+skip_frames = 1
 
-# Show pins
-for nest in nests :
-    plt.scatter(nest.position[0], nest.position[1])
+running = True
+while running:
+    pygame.time.wait(5)
+    # Update everything
+    for i in range(skip_frames):
+        for ant in ants :
+            ant.march()
 
-data = [[],[]]
-datatwo = [[],[]]
-l, = plt.plot([], [])
-ltwo, = plt.plot([], [])
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.xlabel('x')
-plt.title('test')
-line_ani = animation.FuncAnimation(fig1, step_world, 100, fargs=(ants),
-    interval=50, blit=False, repeat = False)
-#line_ani.save('lines.mp4')
-plt.show()
+    # Redraw
+    screen.fill((0,0,0))
+    for ant in ants :
+        pygame.draw.lines(screen, ant.color, False, ant.path, 5)
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
