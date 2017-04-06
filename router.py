f120d335ec2178ebaba45e07cc4a006c77e8b93c
Alex Vierich <avierich@gmail.com>
2017-04-05 20:57:04 -0400

    More refined descisions

--- router.py
+++ router.py
@@ -2,8 +2,8 @@ import numpy as np
 import pygame
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-GRID_WIDTH = 40
-GRID_HEIGHT = 40
+GRID_WIDTH = 80
+GRID_HEIGHT = 80
 RECT_WIDTH = SCREEN_WIDTH / GRID_WIDTH
 RECT_HEIGHT = SCREEN_HEIGHT / GRID_HEIGHT
 
@@ -27,7 +27,9 @@ class Ant :
         self.success = False
         self.age = 0
         self.coords = []
-        self.direction = NORTH
+        self.direction = -999
+        self.age = 0
+        self.world[self.position[0]][self.position[1]].append(self)
 
 
     def kill(self) :
@@ -48,9 +50,11 @@ class Ant :
 
         self.direction = direction
         self.world[self.position[0]][self.position[1]].append(self)
+        self.coords.append(self.position)
 
     def otherAnt(self, coord) :
-        return len(self.world[coord[0]][coord[1]]) > 0 and self.world[coord[0]][coord[1]][0].species != self.species
+        #return len(self.world[coord[0]][coord[1]]) > 0 and self.world[coord[0]][coord[1]][0].species != self.species
+        return len(self.world[coord[0]][coord[1]]) > 0
 
     def canMove(self, direction) :
         if direction == NORTH : 
@@ -65,6 +69,10 @@ class Ant :
             return False
 
     def march(self) :
+        self.age += 1
+        if self.age > 200 :
+            self.kill()
+
         direction = np.array([self.destination.position[0] - self.position[0], self.destination.position[1] - self.position[1]])
         direction = direction / np.sqrt(np.sum(direction**2))
 
@@ -93,6 +101,14 @@ class Ant :
                 secondaryDirection = WEST
 
         teriaryDirection = self.direction
+
+        # Hack
+        things = [0,1,2,3]
+        things.remove(primaryDirection)
+        things.remove(secondaryDirection)
+        if teriaryDirection in things :
+            things.remove(teriaryDirection)
+
         # Lets go for it
         if self.canMove(primaryDirection) :
             self.move(primaryDirection)
@@ -100,6 +116,12 @@ class Ant :
             self.move(secondaryDirection)
         elif self.canMove(teriaryDirection) :
             self.move(teriaryDirection)
+        elif self.canMove(things[0]) :
+            self.move(things[0])
+        elif len(things) > 1 and self.canMove(things[1]) :
+            self.move(things[1])
+        else :
+            self.kill()
             
 
 class Nest :
@@ -127,7 +149,6 @@ class Nest :
             if ant.dead :
                 self.ants.remove(ant)
                 self.newAnt()
-                self.ants[-1].mutate(ant.genes)
             elif not ant.success and ant.position == ant.destination.position :
                 ant.success = True
 #                for i in range(5) :
@@ -135,6 +156,12 @@ class Nest :
 #                        self.newAnt(ant = ant)
             elif not ant.success :
                 ant.march()
+
+def isNest(x, y, nests) :
+    for nest in nests :
+        if nest.position == (x,y) :
+            return True
+    return False
 
 # Setting everything up and run
 
@@ -153,17 +180,40 @@ for i in range(GRID_WIDTH) :
 nests = []
 nests.append(Nest(world, (int(GRID_WIDTH / 4),int(GRID_HEIGHT/2)), 0))
 nests.append(Nest(world, (int(GRID_WIDTH *3/4), int(GRID_HEIGHT /2)), 0))
-nests.append(Nest(world, (int(GRID_WIDTH /2), int(GRID_HEIGHT/4)), 1))
-nests.append(Nest(world, (int(GRID_WIDTH/2), int(GRID_HEIGHT *3/4)), 1))
+nests.append(Nest(world, (int(GRID_WIDTH / 4)+5, int(GRID_HEIGHT/4)), 1))
+nests.append(Nest(world, (int(GRID_WIDTH / 4)+5, int(GRID_HEIGHT *3/4)), 1))
+nests.append(Nest(world, (int(GRID_WIDTH*3 / 4)-5, int(GRID_HEIGHT/4)), 2))
+nests.append(Nest(world, (int(GRID_WIDTH*3 / 4)-5, int(GRID_HEIGHT *3/4)), 2))
 
 nests[0].addConnection(nests[1])
 nests[1].addConnection(nests[0])
 nests[2].addConnection(nests[3])
 nests[3].addConnection(nests[2])
+nests[4].addConnection(nests[5])
+nests[5].addConnection(nests[4])
 
 nests[0].newAnt()
 nests[2].newAnt()
+nests[4].newAnt()
 
+
+nests.append(Nest(world, (int(GRID_WIDTH / 4)-4,int(GRID_HEIGHT/2)), 7))
+nests.append(Nest(world, (int(GRID_WIDTH *3/4)-3, int(GRID_HEIGHT /2)), 7))
+nests.append(Nest(world, (int(GRID_WIDTH / 4)+5-4, int(GRID_HEIGHT/4)), 8))
+nests.append(Nest(world, (int(GRID_WIDTH / 4)+5-4, int(GRID_HEIGHT *3/4)), 8))
+nests.append(Nest(world, (int(GRID_WIDTH*3 / 4)-5-4, int(GRID_HEIGHT/4)), 9))
+nests.append(Nest(world, (int(GRID_WIDTH*3 / 4)-5-4, int(GRID_HEIGHT *3/4)), 9))
+
+nests[0+6].addConnection(nests[1+6])
+nests[1+6].addConnection(nests[0+6])
+nests[2+6].addConnection(nests[3+6])
+nests[3+6].addConnection(nests[2+6])
+nests[4+6].addConnection(nests[5+6])
+nests[5+6].addConnection(nests[4+6])
+
+nests[0+6].newAnt()
+nests[2+6].newAnt()
+nests[4+6].newAnt()
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -179,13 +229,18 @@ while running:
 
     # Redraw
     if frame % skip_frames == 0 : # skip drawing frames
-        pygame.time.wait(200)
+        pygame.time.wait(50)
         pygame.display.set_caption(str(frame))
         screen.fill((0,0,0))
         for x in range(len(world)) :
             for y in range(len(world[0])) :
-                if len(world[x][y]) > 0 and isinstance(world[x][y][0], Ant):
-                    pygame.draw.rect(screen, world[x][y][0].color, (x*RECT_WIDTH, y*RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT), 0)
+                if isNest(x, y, nests) :
+                    pygame.draw.rect(screen, (0,0,255), (x*RECT_WIDTH, y*RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT), 0)
+                elif len(world[x][y]) > 0 and isinstance(world[x][y][0], Ant):
+                    if (x,y) == world[x][y][0].position :
+                        pygame.draw.rect(screen, (255,255,255), (x*RECT_WIDTH, y*RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT), 0)
+                    else :
+                        pygame.draw.rect(screen, world[x][y][0].color, (x*RECT_WIDTH, y*RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT), 0)
                 elif 1 in world[x][y] :
                     pygame.draw.rect(screen, (255, 0, 0), (x*RECT_WIDTH, y*RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT), 0)
 
